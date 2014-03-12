74261a4e257b786cca745a505935824bc3a5e03b
Chris Dawson <xrdawson@gmail.com>
2014-03-11 20:28:40 -0700

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plbh::Application.config.secret_key_base = 'bc6097f422bb50bfddb564ea076b8851c6f25c1c0a1cf51ddc4d7162f9445c3c54d70124e0130891488de7912ff1cc67836338df4b0c78105663eaae46078555'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Plbh::Application.config.session_store :cookie_store, key: '_plbh_session'
