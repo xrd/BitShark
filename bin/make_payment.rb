23c964446a0507702a03894f600787ad2f5e2a88
Chris Dawson <xrdawson@gmail.com>
2014-03-16 01:16:22 -0700

    Add search box for loans, better filtering by date

--- bin/make_payment.rb
+++ bin/make_payment.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'curb'
+require 'json'
 
 code = ARGV.shift
 payment = ARGV.shift
@@ -13,8 +14,13 @@ end
 id = (rand()*100000000).to_i
 
 # params['order']['total_native']['cents']/100, params["order"]["transaction"]["id"] 
+json = ( {order: { total_native: { cents: payment }, transaction: { id: id } } } ).to_json
 
-http = Curl.post("http://sharkbit.eqne.ws/payment/#{code}", {:order => { :total_native => { cents: payment }, transaction: { id: id } } } )
+http = Curl.post("http://sharkbit.eqne.ws/payment/#{code}", json ) do |curl|
+  curl.headers['Accept'] = 'application/json'
+  curl.headers['Content-Type'] = 'application/json'
+  curl.headers['Api-Version'] = '2.2'
+end
 puts http.body_str
 
 
