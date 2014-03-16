266710f33ca43c43edd22790807e9db059b49c4c
Chris Dawson <xrdawson@gmail.com>
2014-03-16 04:32:47 -0400

    Add payment processing

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -5,10 +5,10 @@ class PaymentController < ApplicationController
   def payment_received
     loan = Loan.find_by_code params[:code]
     if loan
-      cents = params['order']['total_native']['cents'].to_i
-      loan.payment!( cents/100, params["order"]["transaction"]["id"] )
+      cents = params[:order][:total_native][:cents].to_i
+      loan.payment!( cents/100, params[:order][:transaction][:id] )
     end
-    render json: loan
+    render json: loan.reload
   end
   
   def button
