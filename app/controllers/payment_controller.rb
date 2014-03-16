a7eefaa5ff2c5a264d83337dbd33bcfd24a6c500
Chris Dawson <xrdawson@gmail.com>
2014-03-16 00:23:28 -0700

    Add payment code

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,7 +1,10 @@
 class PaymentController < ApplicationController
   def payment_received
-    code = params[:code][4..-1]
-    loan = Sponsor
+    loan = Loan.find_by_code params[:code]
+    json = JSON.parse( response.body )
+    if loan
+      loan.payment!( json['order']['total_native']['cents']/100, @current_user.id )
+    end
   end
   
   def button
